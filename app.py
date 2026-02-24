3e6d7dec7a744debb621a15216f0f72b5599a6f3
RugmaBiju <rugmabiju@gmail.com>
2026-02-24 09:29:58 +0530

    AppId updates

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-	return"<h1> Rugma Biju</h1>"
+	return"<h1> Rugma Biju</h1><br> 2409810 </br>"
 
-@app.route("/resume")
-def resume:
-	return "<h1> (resume.html)</h1>
+
 
 if __name__ == "__main__":
 	app.run(host = "0.0.0.0", port = 5000, debug= True)
